ca24bace65c459e76756d2021a409c56f97b7d56
Soner Cafer <sonercafer@outlook.com>
2021-12-28 20:17:06 +0300

    first commit

--- HepsiMarsApp/Program.cs
+++ HepsiMarsApp/Program.cs
@@ -8,11 +8,11 @@ namespace HepsiMarsApp
     {
         static void Main(string[] args)
         {
-            var firstRover = new RoverVehicle(1, 2, Direction.N, new Plateau(5, 5));
+            var firstRover = new RoverManager(1, 2, Direction.N, new Plateau(5, 5));
             firstRover.Command("LMLMLMLMM");
             firstRover.WritePosition();
 
-            var secondRover = new RoverVehicle(3, 3, Direction.E, new Plateau(5, 5));
+            var secondRover = new RoverManager(3, 3, Direction.E, new Plateau(5, 5));
             secondRover.Command("MMRMMRMRRM");
             secondRover.WritePosition();
 
